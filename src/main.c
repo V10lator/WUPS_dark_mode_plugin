1dde33bca83661c785d21d7a890e17bbe33eddcd
Thomas Rohloff <v10lator@myway.de>
2020-08-05 12:28:52 +0200

    Add debugging.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <wups.h>
 #include <coreinit/title.h>
 
+#ifdef WUPS_DM_DEBUG
+    #include <nsysnet/socket.h>
+    #include <utils/logger.h>
+#endif
+
 #define WII_U_MENU_TITLE_ID_JAP (0x0005001010040000)
 #define WII_U_MENU_TITLE_ID_EUR (0x0005001010040200)
 #define WII_U_MENU_TITLE_ID_USA (0x0005001010040100)
@@ -18,14 +23,27 @@ WUPS_ALLOW_KERNEL()
 
 // Gets called once the loader exists.
 ON_APPLICATION_START(args){
+#ifdef WUPS_DM_DEBUG
+    socket_lib_init();
+    log_init();
+    DEBUG_FUNCTION_LINE("ON_APPLICATION_START()!\n");
+#endif
+
     if(!args.kernel_access)
         return;
+
+#ifdef WUPS_DM_DEBUG
+    DEBUG_FUNCTION_LINE("Kernel access!\n");
+#endif
 
     switch(OSGetTitleID())
     {
         case WII_U_MENU_TITLE_ID_EUR:
         case WII_U_MENU_TITLE_ID_USA:
         case WII_U_MENU_TITLE_ID_JAP:
+#ifdef WUPS_DM_DEBUG
+            DEBUG_FUNCTION_LINE("Patching!\n");
+#endif
             WUPS_KernelWrite(WII_U_ADDRESS, 0x3C800000);  
     }
 }
