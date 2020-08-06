0b0e9578bb308be3cf3c9d3d535f3301bb684764
Thomas Rohloff <v10lator@myway.de>
2020-08-06 09:35:38 +0200

    Search memory region at runtime.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/main.c
+++ src/main.c
@@ -10,8 +10,6 @@
 #define WII_U_MENU_TITLE_ID_EUR (0x0005001010040200)
 #define WII_U_MENU_TITLE_ID_USA (0x0005001010040100)
 
-#define WII_U_ADDRESS (void *)0x105DD2A8
-
 // Mandatory plugin information.
 WUPS_PLUGIN_NAME("Dark Mode");
 WUPS_PLUGIN_DESCRIPTION("Wii U Menu dark mode");
@@ -20,6 +18,48 @@ WUPS_PLUGIN_AUTHOR("JacquesCedric & V10lator");
 WUPS_PLUGIN_LICENSE("GPLv3");
 
 WUPS_ALLOW_KERNEL()
+
+static inline void darkenU()
+{
+#ifdef WUPS_DM_DEBUG
+	DEBUG_FUNCTION_LINE("Searching address...\n");
+#endif
+	
+	uint32_t addy = 0x105DD000;
+	uint32_t a;
+	bool found = false;
+	for(uint32_t i = addy; i < 0x10600000; i += 0x00000004)
+	{
+		a = WUPS_KernelRead((void *)i);
+		if(a != 0x3F800000)
+			continue;
+		
+		a = WUPS_KernelRead((void *)(i - 0x00000004));
+		if(a != 0x00000000)
+			continue;
+		
+		a = WUPS_KernelRead((void *)(i + 0x00000004));
+		if(a != 0x40000000)
+			continue;
+		
+		addy = i;
+		found = true;
+		break;
+	}
+	
+	if(!found)
+	{
+#ifdef WUPS_DM_DEBUG
+		DEBUG_FUNCTION_LINE("Not found!\n");
+#endif
+		return;
+	}
+	
+#ifdef WUPS_DM_DEBUG
+	DEBUG_FUNCTION_LINE("Patching!\n");
+#endif
+	WUPS_KernelWrite((void *)addy, 0x3C800000);
+}
 
 // Gets called once the loader exists.
 ON_APPLICATION_START(args){
@@ -41,9 +81,6 @@ ON_APPLICATION_START(args){
         case WII_U_MENU_TITLE_ID_EUR:
         case WII_U_MENU_TITLE_ID_USA:
         case WII_U_MENU_TITLE_ID_JAP:
-#ifdef WUPS_DM_DEBUG
-            DEBUG_FUNCTION_LINE("Patching!\n");
-#endif
-            WUPS_KernelWrite(WII_U_ADDRESS, 0x3C800000);  
+            darkenU();
     }
 }
