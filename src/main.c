729b218a2a5a417279fc4a5b00a72c2e69a6d1e5
Thomas Rohloff <v10lator@myway.de>
2020-08-21 16:57:40 +0200

    v1.1!
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@
 // Mandatory plugin information.
 WUPS_PLUGIN_NAME("Dark Mode");
 WUPS_PLUGIN_DESCRIPTION("Wii U Menu dark mode");
-WUPS_PLUGIN_VERSION("v1.0");
+WUPS_PLUGIN_VERSION("v1.1");
 WUPS_PLUGIN_AUTHOR("JacquesCedric & V10lator");
 WUPS_PLUGIN_LICENSE("GPLv3");
 
